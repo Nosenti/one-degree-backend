ac3d5c25cb1ddf8f64fd1aa1282e818cc25fe3bc
SimonImanigirimpuhwe <softimag08@gmail.com>
2022-03-05 02:57:45 +0200

    ft(chat): add chatting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import express from 'express';
+import http from 'http';
+import { Server } from "socket.io";
+
+
 import router from './routes/index.js';
 import connectDB from './config/db.js';
+
 import 'dotenv/config.js';
+import { handshake, userConnection } from './libs/chat.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+export const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
 
 connectDB()
 
@@ -16,5 +29,9 @@ app.use('/api', router)
 app.listen(PORT, () => {
     console.log('Server has started at port', PORT);
 });
+
+
+//chat handler
+io.use(handshake).on("connection", userConnection);
 
 export default app;
